0c0e240b118937d13e04e33a6a48fa4ce46f2861
Restrorainer <mirco.dickhaut@googlemail.com>
2025-02-16 11:51:43 +0100

    B: added word count

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 def main():
 	with open("books/frankenstein.txt") as f:
-   		file_contents = f.read() 
+		file_contents = f.read() 
 	print(file_contents)
+
+	words = file_contents.split()
+	print(len(words))
+
+
 main()
